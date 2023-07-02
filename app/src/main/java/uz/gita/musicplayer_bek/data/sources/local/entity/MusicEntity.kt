da0ab7424fd764572f377247de0e5d8c150f335d
DAVID <nabijonovo23@gmail.com>
2023-07-02 16:26:28 +0300

    project updated

--- app/src/main/java/uz/gita/musicplayer_bek/data/sources/local/entity/MusicEntity.kt
+++ app/src/main/java/uz/gita/musicplayer_bek/data/sources/local/entity/MusicEntity.kt
@@ -1,17 +1,24 @@
 package uz.gita.musicplayer_bek.data.sources.local.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import uz.gita.musicplayer_bek.data.model.MusicData
 
-@Entity(tableName = "musics")
+@Entity(tableName = "musics",
+    /*foreignKeys = [ForeignKey(entity = PlayListEntity::class,
+    parentColumns = arrayOf("id"),
+    childColumns = arrayOf("playlistId"),
+    onDelete = ForeignKey.CASCADE)]*/
+)
 data class MusicEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val artist: String?,
     val title: String?,
     val data: String?,
-    val duration: Long
+    val duration: Long,
+    val playlistId: Int = 1
 ) {
     fun toData() = MusicData(id, artist, title, data, duration)
 }
